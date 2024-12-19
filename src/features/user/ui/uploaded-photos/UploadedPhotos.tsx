4791f8765248ed8c15f2c1bcbf0a0c12ed6fa6a7
fireonex <mari.krulikovskaya@mail.ru>
2024-12-19 17:41:46 +0300

    IN-287 feat: add cursor pagination and change users table styles

--- src/features/user/ui/uploaded-photos/UploadedPhotos.tsx
+++ src/features/user/ui/uploaded-photos/UploadedPhotos.tsx
@@ -1,41 +1,57 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-import { GET_POSTS_BY_USER } from '@/features/user/api/postsQuery'
-import { useQuery } from '@apollo/client'
-import { Alert, Loader, Typography } from '@byte-creators/ui-kit'
-import { useRouter } from 'next/router'
+import { useUploadedPhotos } from '@/features/user/model/useUploadPhotos'
+import { Alert, Button, Loader, Typography } from '@byte-creators/ui-kit'
 
 import style from './uploadedPhotos.module.scss'
 
 export const UploadedPhotos = () => {
-  const { query } = useRouter()
-  const { data, error, loading } = useQuery(GET_POSTS_BY_USER, {
-    variables: { userId: Number(query.id) },
-  })
+  const {
+    error,
+    hasMore,
+    isFetchingMore,
+    loadMore,
+    loading,
+    posts,
+    query,
+    setEndCursorId,
+    setHasMore,
+    setPosts,
+  } = useUploadedPhotos()
 
-  if (data?.getPostsByUser.items?.length === 0) {
-    return <p>No Uploaded Photos</p>
+  useEffect(() => {
+    setPosts([])
+    setEndCursorId(null)
+    setHasMore(true)
+  }, [query.id])
+
+  if (loading && posts.length === 0) {
+    return <Loader />
   }
-  //TODO: добавить пагинацию и добавить недефолтное число постов
-  // и пофиксить вёрстку таблицы отображения пользователей (Kathaerel)
+  if (error) {
+    return <Alert message={error.message} type={'error'} />
+  }
+  if (!loading && posts.length === 0) {
+    return <Typography>No Uploaded Photos</Typography>
+  }
 
   return (
     <div>
-      {loading && <Loader />}
-      {!loading && !data?.getPostsByUser.items ? (
-        <Typography> User has no publications yet </Typography>
-      ) : (
-        <ul className={style.uploadedPhotosList}>
-          {data?.getPostsByUser.items?.map((post, ind) => {
-            return (
-              <li key={ind}>
-                <img height={260} src={post.url ?? undefined} width={260} />
-              </li>
-            )
-          })}
-        </ul>
+      <ul className={style.uploadedPhotosList}>
+        {posts.map(post => (
+          <li key={post.id}>
+            <img height={260} src={post.url} width={260} />
+          </li>
+        ))}
+      </ul>
+      {isFetchingMore && <Loader />}
+      {!isFetchingMore && hasMore && (
+        <div className={style.buttonContainer}>
+          <Button onClick={loadMore} variant={'outline'}>
+            Load More
+          </Button>
+        </div>
       )}
-      {error && <Alert message={error.message} type={'error'} />}
     </div>
   )
 }
