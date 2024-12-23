376e77c0ec9030b53533b081f831389fbca1e531
tapuru <pogladtapira@gmail.com>
2024-12-23 14:17:31 +0500

    IN-290 refactor: move users sort const to sort entity

--- src/entities/sort/index.ts
+++ src/entities/sort/index.ts
@@ -1,2 +1,3 @@
 export { SortStore } from "./model/sort.store"
 export { TableSortButton } from "./ui/TableSortButton"
+export { USERS_SORT_BY } from "./lib/consts"
---
+++ src/entities/sort/lib/consts.ts
@@ -0,0 +1,6 @@
+export const USERS_SORT_BY = {
+  Amount: 'amount',
+  DateAdded: 'createdAt',
+  PaymentType: 'paymentType',
+  UserName: 'userName',
+} as const
