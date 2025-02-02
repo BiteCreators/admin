d63c5283543bebbf5ebd54cde8754e4c2f99eb28
DePacos <depacos@yandex.ru>
2025-02-02 15:47:35 +0300

    feat: added ws link

--- src/common/api/client.ts
+++ src/common/api/client.ts
@@ -1,8 +1,27 @@
-import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink } from '@apollo/client'
+import { ApolloClient, ApolloLink, InMemoryCache, createHttpLink, split } from '@apollo/client'
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions'
+import { getMainDefinition } from '@apollo/client/utilities'
+import { createClient } from 'graphql-ws'
 
 const httpLink = createHttpLink({
   uri: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
 })
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: process.env.NEXT_PUBLIC_GRAPHQL_WS_API_URL as string,
+  })
+)
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query)
+
+    return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
 
 const authLink = new ApolloLink((operation, forward) => {
   const accessToken = document.cookie.split('adminAccessToken=')[1]
@@ -24,5 +43,5 @@ const authLink = new ApolloLink((operation, forward) => {
 
 export const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: authLink.concat(httpLink),
+  link: authLink.concat(splitLink),
 })
